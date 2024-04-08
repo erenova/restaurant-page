ad481df416c7d586ddb68d53ac2ce504fa058c6b
erenova <erenova6@gmail.com>
2024-04-08 07:13:54 +0300

    update prices of menu items

--- src/modules/product.js
+++ src/modules/product.js
@@ -5,7 +5,7 @@ export class product {
     grams = ["", "", ""],
     prices = ["", "", ""],
     type = "dish",
-    info = ""
+    info = "",
   ) {
     this.title = title;
     (this.imageSrc = imageSrc), (this.grams = grams), (this.prices = prices);
@@ -19,35 +19,35 @@ export const productsList = [
     ["Half Bread", "Yarım Ekmek"],
     "itemHalf.svg",
     ["65", "90", "115"],
-    ["120", "165", "210"]
+    ["150", "205", "260"],
   ),
   new product(
     ["Whole Bread", "Tam Ekmek"],
     "itemWhole.svg",
     ["100", "140", "180"],
-    ["185", "260", "330"]
+    ["235", "325", "415"],
   ),
   new product(
     [`Quarter Bread`, `Çeyrek Ekmek`],
     "itemQuarter.svg",
     ["40", "55", "70"],
-    ["70", "100", "130"]
+    ["90", "125", "160"],
   ),
   new product(
     [`On Plate`, "Tabak"],
     "itemPlate.svg",
     ["110", "150", "190"],
-    ["205", "275", "345"]
+    ["255", "345", "435"],
   ),
 
-  new product(["Cola", "Kola"], "cola.svg", "", "25", "beverage", "330 ml"),
-  new product(["Fanta", "Fanta"], "fanta.svg", "", "25", "beverage", "330 ml"),
-  new product(["Turnip Juice", "Şalgam"], "salgam.svg", "", "15", "beverage"),
-  new product("Ayran", "ayranBuyuk1.svg", "", "15", "beverage", [
+  new product(["Cola", "Kola"], "cola.svg", "", "35", "beverage", "330 ml"),
+  new product(["Fanta", "Fanta"], "fanta.svg", "", "35", "beverage", "330 ml"),
+  new product(["Turnip Juice", "Şalgam"], "salgam.svg", "", "20", "beverage"),
+  new product("Ayran", "ayranBuyuk1.svg", "", "25", "beverage", [
     `(Big)`,
     `(Büyük)`,
   ]),
-  new product("Ayran", "ayranKucuk1.svg", "", "10", "beverage", [
+  new product("Ayran", "ayranKucuk1.svg", "", "15", "beverage", [
     `(Small)`,
     `(Küçük)`,
   ]),
@@ -55,20 +55,20 @@ export const productsList = [
     ["Mineral Water", "Sade Soda"],
     "sodaSade.svg",
     "",
-    "10",
-    "beverage"
+    "15",
+    "beverage",
   ),
   new product(
     ["Lemon Soda", "Limonlu Soda"],
     "sodaLimon.svg",
     "",
-    "10",
-    "beverage"
+    "20",
+    "beverage",
   ),
-  new product([`Juice `, "Meyve Suyu"], "juice.svg", "", "10", "beverage", [
+  new product([`Juice `, "Meyve Suyu"], "juice.svg", "", "15", "beverage", [
     `(Sourcherry/
     Peach)`,
     ``,
   ]),
-  new product(["Water", "Su"], "su.svg", "", "5", "beverage", "(0.5 L)"),
+  new product(["Water", "Su"], "su.svg", "", "10", "beverage", "(0.5 L)"),
 ];
